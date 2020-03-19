3ca8622cb828f1e47bc91f15a01f6dfffb998c34
Ritesh <riteshdharmatti@gmail.com>
2020-03-19 11:22:11 -0700

    n-th node from end of the linked list

--- src/main/java/org/rd/App.java
+++ src/main/java/org/rd/App.java
@@ -18,7 +18,48 @@ public class App
 
         printLL(ll);
 
+        int n = 2;
+
         System.out.println("Middle of the Linked List : "+middleOfLL(ll));
+
+        System.out.println("n-th node from the end : "+nthNodeFromEndLengthApproachBigOLinear(ll,n));
+
+        System.out.println("n-th node from the end : "+nthNodeFromEndTwoPointerApproachBigOLinear(ll,n));
+    }
+
+    private static int nthNodeFromEndTwoPointerApproachBigOLinear(LinkedList ll, int n) {
+        Node firstNode = ll.head;
+        Node secondNode = ll.head;
+        for(int i=0;i<n;i++){
+            if(firstNode == null){
+                return -1;
+            }
+            firstNode = firstNode.next;
+        }
+        while(firstNode!=null){
+            secondNode = secondNode.next;
+            firstNode = firstNode.next;
+        }
+
+        return secondNode.data;
+    }
+
+    private static int nthNodeFromEndLengthApproachBigOLinear(LinkedList ll, int n) {
+        int length = 0;
+        Node curr = ll.head;
+        while(curr!=null){
+            curr = curr.next;
+            length++;
+        }
+        System.out.println("Length of linked list:"+length);
+        if(length<n){
+            return -1;
+        }
+        curr=ll.head;
+        for(int i=1;i<length-n+1;i++){
+            curr = curr.next;
+        }
+        return curr.data;
     }
 
     private static void printLL(LinkedList ll) {
@@ -41,10 +82,11 @@ public class App
             if(size >=1){
                 size = size/2;
             }
+            Node curr = linkedList.head;
             for(int i=0;i<size;i++){
-                linkedList.head = linkedList.head.next;
+                curr = curr.next;
             }
-            return linkedList.head.data;
+            return curr.data;
         }
     }
 
